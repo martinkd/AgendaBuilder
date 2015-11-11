10c59cf1b4dd54cfc2cd2e87a52e5196fbb83548
Martin Dang <martin.k.dang@gmail.com>
2015-11-11 20:14:59 +0200

    Fixes #1 testing creating new event method, show all events, read event by ID

--- AgendaBuilder/src/testClass.java
+++ AgendaBuilder/src/testClass.java
@@ -1,10 +1,17 @@
+import java.text.ParseException;
 
 public class testClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	}
+	public static void main(String[] args) throws ParseException {
 
+	AgendaBuilderOperator operator = new AgendaBuilderOperator();
+	String txtDateStart = "15/02/2015";
+	String txtDateEnd = "18/02/2015";
+	operator.createEvent(2, "martin", "BG", "BS", txtDateStart, txtDateEnd, true);
+	operator.createEvent(5, "gosho", "Vn", "hanoi", txtDateStart, txtDateEnd, true);
+	operator.createEvent(4, "sunny", "bg", "Pld", txtDateStart, txtDateEnd, true);
+	operator.showAllEvents();
+	operator.readEvent(4);
+	}
 }
 	
