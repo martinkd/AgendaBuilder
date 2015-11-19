4daecc25a624e40c0fd07090ae15a3a93cedf00c
Martin Dang <martin.k.dang@gmail.com>
2015-11-19 13:09:41 +0200

    new Operator class

--- AgendaBuilder/src/Operator.java
+++ AgendaBuilder/src/Operator.java
@@ -1,33 +1,46 @@
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
-import java.util.InputMismatchException;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.Map.Entry;
 
 public class Operator {
 	private static Map<Integer, Event> listOfEvents = new HashMap<Integer, Event>();
 
-	public static void createNewEvent() {
-		Scanner input = new Scanner(System.in);
+	public static void createNewEvent(Scanner input) {
 		Event newEvent = new Event();
-		System.out.println("Enter Id: ");
-		newEvent.setId(getNextInt(input));
-		System.out.print("Enter name: ");
-		newEvent.setName(input.next());
-		System.out.print("Enter country: ");
-		newEvent.setCountry(input.next());
-		System.out.print("Enter location: ");
-		newEvent.setLocation(input.next());
-		setStartDate(input, newEvent);
-		setEndDate(input, newEvent);
-		setIsFreeEvent(newEvent);
-		System.out.println("You sucessfully created new event\n");
-		AgendaBuilderMenu.operatorMenu();
-	}
+		System.out.print("Id: ");
+		int id = getValidInteger(input);
+		if (!listOfEvents.containsKey(id)) {
+			newEvent.setId(id);
+		} else {
+			System.err.printf("Event with Id: %s already exists, please enter another Id%n", id);
+			System.out.println();
+			createNewEvent(input);
+		}
 
-	private static int getNextInt(Scanner input) {
+		System.out.print("Enter name: ");
+		String name = input.nextLine();
+		newEvent.setName(name);
+		System.out.print("Enter country: ");
+		newEvent.setCountry(input.nextLine());
+		System.out.print("Enter location: ");
+		newEvent.setLocation(input.nextLine());
+		System.out.print("Enter start date: ");
+		newEvent.setStartDate(validDate(input));
+		System.out.print("Enter end date: ");
+		newEvent.setEndDate(validDate(input));
+		System.out.print("Enter \"true\" if the event is free: ");
+		newEvent.setIsFreeEvent(getValidBoolean(input));
+		listOfEvents.put(id, newEvent);
+		System.out.printf("%nYou sucsesfully created event \"%s\" %n", name);
+		System.out.println();
+	}
+	
+	private static int getValidInteger(Scanner input) {
 		while (true) {
 			try {
 				String txtNum = input.nextLine();
@@ -37,53 +50,39 @@ public class Operator {
 			}
 		}
 	}
-
-	private static void setIsFreeEvent(Event newEvent) {
-		System.out.print("Is free event(true/false): ");
+	
+	private static Date validDate(Scanner input) {
+		DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+		dateFormat.setLenient(false);
+		String dateAsString = input.nextLine();
 		try {
-			Scanner scan = new Scanner(System.in);
-			newEvent.setIsFreeEvent(scan.nextBoolean());
-			scan.close();
-		} catch (InputMismatchException e) {
-			System.err.println("Enter \"true\" or \"false\"");
-			setIsFreeEvent(newEvent);
-		}
-	}
-
-	private static void setStartDate(Scanner input, Event newEvent) {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-		System.out.print("Enter start date(dd.mm.yyyy): ");
-		try {
-			newEvent.setStartDate(sdf.parse(input.next()));
+			Date date = dateFormat.parse(dateAsString);
+			return date;
 		} catch (ParseException e) {
 			System.err.println("Enter valid date format (dd.mm.yyyy)!");
-			setStartDate(input, newEvent);
+			return validDate(input);
 		}
 	}
-
-	private static void setEndDate(Scanner input, Event newEvent) {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-		System.out.print("Enter end date(dd.mm.yyyy): ");
-		try {
-			newEvent.setEndDate(sdf.parse(input.next()));
-		} catch (ParseException e) {
-			System.err.println("Enter valid date format (dd.mm.yyyy)!");
-			setEndDate(input, newEvent);
+	
+	private static boolean getValidBoolean(Scanner input) {
+		while (true) {
+			try {
+				String txtNum = input.nextLine();
+				return Boolean.parseBoolean(txtNum);
+			} catch (NumberFormatException e) {
+				System.err.println("Enter valid number");
+			}
 		}
 	}
-
+	
 	public static void showAllEvents() {
 		for (Entry<Integer, Event> event : listOfEvents.entrySet()) {
 			System.out.printf("Id: %s, Name: %s", event.getValue().getId(), event.getValue().getName());
+			System.out.println();
 		}
 	}
-
-	public void readEvent(int id) {
-		System.out.println(listOfEvents.get(id));
-	}
-
-	public void editEvent(int id) {
-		if (listOfEvents.containsKey(id)) {
-		}
+	
+	public static void readEvent(Scanner input) {
+		System.out.println(listOfEvents.get(getValidInteger(input)));
 	}
 }
